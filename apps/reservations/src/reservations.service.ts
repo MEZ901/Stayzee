94e4e2bb48d593fc1520d161822527fdc9a559b1
MEZ901 <issammez44@gmail.com>
2024-01-01 19:04:59 +0100

    implement reservation crud functionalities

---
+++ apps/reservations/src/reservations.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationsRepository } from './reservations.repository';
+
+@Injectable()
+export class ReservationsService {
+  constructor(private readonly reservationRepository: ReservationsRepository) {}
+
+  async create(createReservationDto: CreateReservationDto) {
+    return await this.reservationRepository.create({
+      ...createReservationDto,
+      timestamp: new Date(),
+      userId: '123',
+    });
+  }
+
+  async findAll() {
+    return await this.reservationRepository.find({});
+  }
+
+  async findOne(_id: string) {
+    return await this.reservationRepository.findOne({ _id });
+  }
+
+  async update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return await this.reservationRepository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  async remove(_id: string) {
+    return await this.reservationRepository.findOneAndDelete({ _id });
+  }
+}
